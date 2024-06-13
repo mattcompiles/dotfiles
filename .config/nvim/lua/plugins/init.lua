091db583dbd618043bd1a29ee166159031285a2d
mattcompiles <mattjones701@gmail.com>
2024-06-13 21:05:48 +1000

    Clean up nvim config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -11,57 +11,6 @@ return {
   },
   "jeffkreeftmeijer/vim-numbertoggle",
   {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        path_display = { "truncate" },
-      },
-    },
-    keys = {
-      -- disable the keymap to grep files
-      -- { "<leader>/", false },
-      -- change a keymap
-      { "<leader>fr", LazyVim.telescope("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
-      { "<leader>fR", "<cmd>Telescope oldfiles<cr>", desc = "Recent (all)" },
-      {
-        "<leader>fp",
-        function()
-          require("telescope.builtin").find_files({ search_file = "package.json" })
-        end,
-        desc = "Find packages",
-      },
-      {
-        "<leader>fn",
-        function()
-          require("telescope.builtin").find_files({
-            no_ignore = true,
-            search_dirs = { "node_modules" },
-            search_file = "package.json",
-          })
-        end,
-        desc = "Find node_module",
-      },
-      {
-        "<leader>fb",
-        function()
-          require("telescope.builtin").find_files({
-            no_ignore = true,
-            search_dirs = { "build", "jira/build" },
-          })
-        end,
-        desc = "Find build files",
-      },
-      -- add a keymap to browse plugin files
-      -- {
-      --   "<leader>fp",
-      --   function()
-      --     require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-      --   end,
-      --   desc = "Find Plugin File",
-      -- },
-    },
-  },
-  {
     "nvim-neo-tree/neo-tree.nvim",
     keys = {
       {
