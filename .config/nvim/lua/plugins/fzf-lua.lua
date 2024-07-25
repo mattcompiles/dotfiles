e4709952edf547fdc66b9410a70977e4841a2e6f
mattcompiles <mattjones701@gmail.com>
2024-07-25 10:13:03 +1000

    Add fzf-lua config

---
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,83 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader><space>", LazyVim.pick("files"), desc = "Find files - Git" },
+      {
+        "<leader>ff",
+        function()
+          LazyVim.pick("files", { cwd = vim.g.package_root() })()
+        end,
+        desc = "Find files - Package",
+      },
+      {
+        "<leader>fF",
+        function()
+          LazyVim.pick("files", { cwd = vim.g.workspace_root() })()
+        end,
+        desc = "Find files - Workspace root",
+      },
+      { "<leader>fr", LazyVim.pick("oldfiles", { cwd = LazyVim.root.git() }), desc = "Recent files" },
+      { "<leader>fR", false },
+      {
+        "<leader>fn",
+        function()
+          LazyVim.pick("files", { cwd = vim.fs.joinpath(vim.g.workspace_root(), "node_modules") })()
+        end,
+        desc = "Find node_modules",
+      },
+      {
+        "<leader>fb",
+        function()
+          LazyVim.pick("files", { cwd = vim.fs.joinpath(vim.g.workspace_root(), "build") })()
+        end,
+        desc = "Find files - Build",
+      },
+      {
+        "<leader>fp",
+        function()
+          local cmd_string = string.format(
+            'rg \z
+              \'"name": "(.+)"\' \z
+              -g "package.json" \z
+              -r \'$1\' \z
+              %s \z
+              --no-heading \z
+              --no-line-number \z
+              --trim \z
+              --max-count=1',
+            LazyVim.root.git()
+          )
+
+          require("fzf-lua").fzf_exec(cmd_string, {
+            fzf_opts = { ["--delimiter"] = ":", ["--with-nth"] = 2 },
+            fn_transform = function(input)
+              return string.sub(input, 1, -2)
+            end,
+            actions = {
+              ["default"] = function(selection, opts)
+                local package_path = string.gmatch(selection[1], "(.+):(.+)")()
+                require("fzf-lua.actions").file_switch_or_edit({ package_path }, opts)
+              end,
+            },
+          })
+        end,
+        desc = "Find packages",
+      },
+      {
+        "<leader>sg",
+        LazyVim.pick("live_grep", { cwd = LazyVim.root.git() }),
+        desc = "Grep",
+      },
+      {
+        "<leader>sG",
+        function()
+          LazyVim.pick("live_grep", { cwd = vim.g.package_root() })()
+        end,
+        desc = "Grep - Package",
+      },
+      { "<leader>fc", false },
+      { "<leader>fg", false },
+    },
+  },
+}
