3e84ad7446c3af71056a8a95a609536518218a05
mattcompiles <mattjones701@gmail.com>
2025-04-09 11:32:31 +1000

    Fixed path yank

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,7 +9,15 @@ local path_yank = require("utils.path-yank")
 wk.add({ "<leader>cy", group = "Copy file path" })
 wk.add({ "<leader>cyy", path_yank.copy_local_path, mode = { "n", "v" }, desc = "Copy current path (relative)" })
 wk.add({ "<leader>cya", path_yank.copy_absolute_local_path, mode = { "n", "v" }, desc = "Copy current path (absolute)" })
-wk.add({ "<leader>cyr", path_yank.copy_remote_path, mode = { "n", "v" }, desc = "Copy current path (as url)" })
+wk.add({ "<leader>cyb", path_yank.copy_remote_path, mode = { "n", "v" }, desc = "Copy current path (as url)" })
+wk.add({
+  "<leader>cym",
+  function()
+    path_yank.copy_remote_path(true)
+  end,
+  mode = { "n", "v" },
+  desc = "Copy current path without branch (as url)",
+})
 
 wk.add({ "<leader>d", group = "Debug utils" })
 wk.add({ "<leader>dl", "Oconsole.log(`<Esc>pf)<Ignore>i,<Space><Esc>p", desc = "log last yank", remap = true })
